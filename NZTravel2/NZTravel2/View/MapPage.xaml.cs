5d91ff55388e275cadada3271b2af5a989746d0c
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-04-05 09:36:13 +1300

    only map showing with current location tried to add unit test

--- NZTravel2/NZTravel2/View/MapPage.xaml.cs
+++ NZTravel2/NZTravel2/View/MapPage.xaml.cs
@@ -11,130 +11,162 @@ using Plugin.Geolocator;
 
 namespace NZTravel2.View
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MapPage : ContentPage
-	{
-		public MapPage ()
-		{
-			InitializeComponent ();
-
-            MapPageLayout();
-        }
-
-        #region intialise
-        Label lblLat = new Label();
-        Label lblLong = new Label();
-        Map map = new Map();
-        #endregion
-
-        private void MapPageLayout()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
         {
-            #region label latitude
-            lblLat = new Label
-            {
-                Text = "Latitude:",
-                FontSize = 20,
-                TextColor = Color.Gray
-            };
-            StackLayout slLblLat = new StackLayout
-            {
-                Children = { lblLat },
-                Orientation = StackOrientation.Horizontal,
-                Padding = new Thickness(10, 10, 10, 10)
-            };
-            #endregion
-
-            #region label longitude
-            lblLong = new Label
-            {
-                Text = "Longitude:",
-                FontSize = 20,
-                TextColor = Color.Gray
-            };
-            StackLayout slLblLong = new StackLayout
-            {
-                Children = { lblLong },
-                Orientation = StackOrientation.Horizontal,
-                Padding = new Thickness(10, 10, 10, 10)
-            };
-            #endregion
-
-            #region button for current location
-            Button btnLatLong = new Button
-            {
-                Text = "Choose Location",
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                BackgroundColor = Color.FromHex("4690FB"),
-                TextColor = Color.White,
-                WidthRequest = 100,
-                HeightRequest = 50,
-                FontAttributes = FontAttributes.Bold
-            };
-            btnLatLong.Clicked += BtnLatLong_Clicked;
-
-            StackLayout slBtnLatLong = new StackLayout
-            {
-                Children = { btnLatLong },
-                Orientation = StackOrientation.Horizontal,
-                Padding = new Thickness(20, 20, 20, 20)
-            };
-            #endregion
-
-            #region stack layouts and contents
-            StackLayout slGoogleMapScreen = new StackLayout
-            {
-                Children = { slLblLat, slLblLong, slBtnLatLong },
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Padding = new Thickness(0, 0, 0, 0),
-                BackgroundColor = Color.FromHex("DDEEFD")
-            };
-
-            ScrollView svGoogleMapScreen = new ScrollView
-            {
-                Content = slGoogleMapScreen
-            };
-
-            Content = svGoogleMapScreen;
-            #endregion
-
-            #region Map
-            map = new Map
-            {
-                WidthRequest = 320,
-                HeightRequest = 200,
-                IsShowingUser = true,
-                MapType = MapType.Street
-            };
-
-            StackLayout slMap = new StackLayout
-            {
-                Children = { map },
-                Orientation = StackOrientation.Horizontal,
-                Padding = new Thickness(10, 10, 10, 10)
-            };
-            #endregion
-
-            async void BtnLatLong_Clicked(object sender, EventArgs e)
-            {
-                await RetrieveLocation();
-            }
-
-            async Task RetrieveLocation()
-            {
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 20;
-                var position = await locator.GetPositionAsync(); 
-
-                lblLat.Text = "Latitude: " + position.Latitude.ToString();
-                lblLong.Text = "Longitude: " + position.Longitude.ToString();
-
-                map.MoveToRegion(
-                    MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
-                    Distance.FromMiles(0.5)));
-            }
+            InitializeComponent();
+            Display();
+            //MapPageLayout();
         }
 
+        async void Display()
+        {
+            await RetrieveLocation();
+        }
 
+        async Task RetrieveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(120));
+
+            Longitude(position.Longitude);
+
+            map.MoveToRegion(
+                MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
+                Distance.FromMiles(0.5)));
+
+            map.Pins.Add(new Pin
+            {
+                Type = PinType.Place,
+                Label = "Current Position",
+                Position = new Position(position.Latitude, position.Longitude)
+            });
+
+        }
+
+        public double Longitude(double longitude)
+        {
+            return longitude;
+        }
+
+        //#region intialise
+        //Label lblLat = new Label();
+        //Label lblLong = new Label();
+        //Map map = new Map();
+        //#endregion
+
+        //private void MapPageLayout()
+        //{
+        //    #region label latitude
+        //    lblLat = new Label
+        //    {
+        //        Text = "Latitude:",
+        //        FontSize = 20,
+        //        TextColor = Color.Gray
+        //    };
+        //    StackLayout slLblLat = new StackLayout
+        //    {
+        //        Children = { lblLat },
+        //        Orientation = StackOrientation.Horizontal,
+        //        Padding = new Thickness(10, 10, 10, 10)
+        //    };
+        //    #endregion
+
+        //    #region label longitude
+        //    lblLong = new Label
+        //    {
+        //        Text = "Longitude:",
+        //        FontSize = 20,
+        //        TextColor = Color.Gray
+        //    };
+        //    StackLayout slLblLong = new StackLayout
+        //    {
+        //        Children = { lblLong },
+        //        Orientation = StackOrientation.Horizontal,
+        //        Padding = new Thickness(10, 10, 10, 10)
+        //    };
+        //    #endregion
+
+        //    #region button for current location
+        //    Button btnLatLong = new Button
+        //    {
+        //        Text = "Choose Location",
+        //        HorizontalOptions = LayoutOptions.FillAndExpand,
+        //        BackgroundColor = Color.FromHex("4690FB"),
+        //        TextColor = Color.White,
+        //        WidthRequest = 100,
+        //        HeightRequest = 50,
+        //        FontAttributes = FontAttributes.Bold
+        //    };
+        //    btnLatLong.Clicked += BtnLatLong_Clicked;
+
+        //    StackLayout slBtnLatLong = new StackLayout
+        //    {
+        //        Children = { btnLatLong },
+        //        Orientation = StackOrientation.Horizontal,
+        //        Padding = new Thickness(20, 20, 20, 20)
+        //    };
+        //    #endregion
+
+        //    #region stack layouts and contents
+        //    StackLayout slGoogleMapScreen = new StackLayout
+        //    {
+        //        Children = { slLblLat, slLblLong, slBtnLatLong },
+        //        HorizontalOptions = LayoutOptions.FillAndExpand,
+        //        VerticalOptions = LayoutOptions.FillAndExpand,
+        //        Padding = new Thickness(0, 0, 0, 0),
+        //        BackgroundColor = Color.FromHex("DDEEFD")
+        //    };
+
+        //    ScrollView svGoogleMapScreen = new ScrollView
+        //    {
+        //        Content = slGoogleMapScreen
+        //    };
+
+        //    Content = svGoogleMapScreen;
+        //    #endregion
+
+        //    #region Map
+        //    map = new Map
+        //    {
+        //        WidthRequest = 320,
+        //        HeightRequest = 200,
+        //        IsShowingUser = true,
+        //        MapType = MapType.Street
+        //    };
+
+        //    StackLayout slMap = new StackLayout
+        //    {
+        //        Children = { map },
+        //        Orientation = StackOrientation.Horizontal,
+        //        Padding = new Thickness(10, 10, 10, 10)
+        //    };
+        //    #endregion
+
+        //    async void BtnLatLong_Clicked(object sender, EventArgs e)
+        //    {
+        //        await RetrieveLocation();
+        //    }
+
+        //    async Task RetrieveLocation()
+        //    {
+        //        var locator = CrossGeolocator.Current;
+        //        locator.DesiredAccuracy = 20;
+        //        var position = await locator.GetPositionAsync();
+
+        //        lblLat.Text = "Latitude: " + position.Latitude.ToString();
+        //        lblLong.Text = "Longitude: " + position.Longitude.ToString();
+
+        //        map.MoveToRegion(
+        //            MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
+        //            Distance.FromMiles(0.5)));
+        //    }
+        //}
+
+
+        //}
     }
 }
---
+++ NZTravelTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NZTravel2.View;
+
+namespace NZTravelTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_GetLongitude()
+        {
+            MapPage nzt2 = new MapPage();
+            var longitude = nzt2.Longitude(174.7673);
+            Assert.AreEqual(longitude, 174.7673);
+        }
+    }
+}
