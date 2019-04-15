8a71d1ba888a2a3602c62d624bf861c9109056d2
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-04-15 14:01:57 +1200

    search functionality works - moves to correct position on map on button click

--- NZTravel2/NZTravel2/View/MapPage.xaml.cs
+++ NZTravel2/NZTravel2/View/MapPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Maps;
 using Plugin.Geolocator;
+using System.Net.Http;
 
 namespace NZTravel2.View
 {
@@ -21,7 +22,6 @@ namespace NZTravel2.View
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             Display();
-            //MapPageLayout();
         }
 
         async void Display()
@@ -43,18 +43,47 @@ namespace NZTravel2.View
                 MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
                 Distance.FromMiles(0.5)));
 
-            map.Pins.Add(new Pin
-            {
-                Type = PinType.Place,
-                Label = "Current Position",
-                Position = new Position(position.Latitude, position.Longitude)
-            });
+            //map.Pins.Add(new Pin
+            //{
+            //    Type = PinType.Place,
+            //    Label = "Current Position",
+            //    Position = new Position(position.Latitude, position.Longitude)
+            //});
 
         }
 
         public double Longitude()
         {          
             return longitude;
+        }
+
+        public void OnSearchButtonPress(object sender, System.EventArgs e)
+        {
+            var text = SearchBar.Text;
+            GetPlace(text);  
+        }
+
+        public async void GetPlace(string text)
+        {
+            await ShowPlace(text);
+        }
+
+        public async Task ShowPlace(string text)
+        {
+            Geocoder gc = new Geocoder();
+            IEnumerable<Position> result = await gc.GetPositionsForAddressAsync(text);
+            foreach (Position pos in result)
+            {
+                map.Pins.Add(new Pin
+                {
+                    Type = PinType.Place,
+                    Label = text,
+                    Position = new Position(pos.Latitude, pos.Longitude)
+                });
+                map.MoveToRegion(
+                    MapSpan.FromCenterAndRadius(new Position(pos.Latitude, pos.Longitude),
+                    Distance.FromMiles(0.5)));
+            }
         }
 
     }
