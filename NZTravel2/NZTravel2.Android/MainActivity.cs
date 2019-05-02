49565216ee345ef3f2111ff4bfb6c0f54d038493
may2708 <43025619+may2708@users.noreply.github.com>
2019-05-03 08:31:45 +1200

    unit tests

--- NZTravel2/NZTravel2.Android/MainActivity.cs
+++ NZTravel2/NZTravel2.Android/MainActivity.cs
@@ -20,6 +20,7 @@ namespace NZTravel2.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
         }
     }
 }
